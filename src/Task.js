800463f9953237548dfe38a68b9d4e07b3d00158
aqshalprawira <aqshal.map@gmail.com>
2023-06-19 04:58:03 +0700

    deploy to netlify

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Muhammad Aqshal Prawira" // TODO: replace this
+const studentId = "FE5622157" // TODO: replace this
+
+export { url, studentName, studentId }
